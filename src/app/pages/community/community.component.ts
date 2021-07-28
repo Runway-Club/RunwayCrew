cd10c6223c6cc211670818c1acad7cd85f4b32c0
Van Huu Gia Kien <vanhuugiakien@gmail.com>
2021-07-28 17:31:27 +0700

    Update list

--- src/app/pages/community/community.component.ts
+++ src/app/pages/community/community.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile.service';
+import { UserProfile } from 'src/models/user-profile.model';
 
 @Component({
   selector: 'app-community',
   templateUrl: './community.component.html',
-  styleUrls: ['./community.component.scss']
+  styleUrls: ['./community.component.scss'],
 })
 export class CommunityComponent implements OnInit {
-
-  constructor() { }
+  public data!: UserProfile[];
+  constructor(private profileSv: ProfileService) {}
 
   ngOnInit(): void {
+    setTimeout(async () => {
+      await this.getAllUser();
+    }, 500);
   }
-
+  public async getAllUser() {
+    await this.profileSv.getAll().subscribe(async (user) => {
+      this.data = user;
+      console.log(this.data);
+    });
+  }
 }
