df221f821664d55aa18e81177e4e358eea310a6e
Van Huu Gia Kien <vanhuugiakien@gmail.com>
2021-07-28 21:21:58 +0700

    Fix profile layout

--- src/app/pages/profile/components/header/header.component.ts
+++ src/app/pages/profile/components/header/header.component.ts
@@ -7,28 +7,27 @@ import { UserContribution } from 'src/models/user-profile.model';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
   exp: number = 0;
   avgas: number = 1028;
   rank: string = 'S';
   usr: any = null;
   contributionInfo: UserContribution | undefined;
-
+  loadDone = false;
   constructor(
     private authService: AuthenticationService,
     private auth: AngularFireAuth,
     private contributionService: ContributionService
-  ) { }
+  ) {}
 
   ngOnInit() {
     setTimeout(async () => {
       await this.getContributions();
       this.usr = await this.authService.user;
-    }, 500)
-
+      this.loadDone = true;
+    }, 500);
   }
 
   async getContributions() {
