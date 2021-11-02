2e5d02470ae4e3e591c5a2c75fed16a6c46ad21a
Đức Trần <hsu.tranhuynhduc@gmail.com>
2021-11-02 19:08:35 +0700

    get hi

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = require('./src/server')
 
 async function main() {
   await Database.instance.connect();
+  app.get('', (req, res) => {
+    res.send('Hi there, server running...');
+  })
   app.listen(config.PORT, config.HOST, () => { console.log(`Server started on ${config.HOST}:${config.PORT}`); })
 }
 
--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
     PORT: '80',
-    // HOST: '0.0.0.0',
-    HOST: 'localhost',
+    HOST: '0.0.0.0',
     CONNECTIONSTRING: 'mongodb+srv://admin:admin@cluster0.n75mo.mongodb.net/Web',
 }
