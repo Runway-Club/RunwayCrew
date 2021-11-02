19706b3e05bf28bedfad0865ac7d0f716a37d260
Đức Trần <hsu.tranhuynhduc@gmail.com>
2021-11-02 15:28:25 +0700

    change host

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: '80',
-    HOST: '0.0.0.0',
+    // HOST: '0.0.0.0',
+    HOST: 'localhost',
     CONNECTIONSTRING: 'mongodb+srv://admin:admin@cluster0.n75mo.mongodb.net/Web',
 }
