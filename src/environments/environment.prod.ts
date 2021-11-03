b9aa4918b02dddc3f7136b6a6489bcea8bdbdc17
Đức Trần <hsu.tranhuynhduc@gmail.com>
2021-11-03 20:40:15 +0700

    hosting server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,5 @@ export const environment = {
     measurementId: "G-GRZ989634P"
   },
   // endpoint:'http://localhost:8080/'
-  endpoint:'http://34.124.115.70/'
+  endpoint:'https://server.crew.runwayclub.dev/'
 };
